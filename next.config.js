fe1ad6af290052cd7b37a6ed1bfe5667528b5d5f
Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>
2024-03-22 22:41:42 +0530

    adde code

--- next.config.js
+++ next.config.js
@@ -1,12 +1,36 @@
+// next.config.js
+import nextConfig from 'next/config';
+
+const {
+  // Retrieve values from the default/previous next.config.js file
+  serverRuntimeConfig,
+  publicRuntimeConfig,
+  ...otherConfig
+} = nextConfig();
+
+// Add your custom configurations here
+const customConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sarkarifiling-assets.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+  experimental: {
+    outputFileTracing: true,
+    outputStandalone: true,
+    esmExternals: true,
+  },
+};
+
+// Merge the default configurations with the custom configurations
 module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'sarkarifiling-assets.s3.ap-south-1.amazonaws.com',
-          port: '',
-          pathname: '/images/**',
-        },
-      ],
-    },
-  }
+  ...otherConfig,
+  ...customConfig,
+  serverRuntimeConfig,
+  publicRuntimeConfig,
+};
