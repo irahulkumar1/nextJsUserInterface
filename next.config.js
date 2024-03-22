f2262d53a0284128c6fbe38c1a86997b7ebd54a9
Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>
2024-03-22 22:36:34 +0530

    config chagne to es6

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
-module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'sarkarifiling-assets.s3.ap-south-1.amazonaws.com',
-          port: '',
-          pathname: '/images/**',
-        },
-      ],
-    },
-  }
-  module.exports = {
-    experimental: {
-      outputFileTracing: true,
-      outputStandalone: true,
-      esmExternals: true,
-    },
-  };
+// next.config.js
+import { defineConfig } from 'next/config';
+
+export default defineConfig({
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sarkarifiling-assets.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+  experimental: {
+    outputFileTracing: true,
+    outputStandalone: true,
+    esmExternals: true,
+  },
+});
