466dd8bf8db506934c5ca8d61d7e9b43a448e4da
Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>
2024-03-22 22:59:50 +0530

    modified package and config

---
+++ next.config.cjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'next/config';
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sarkarifiling-assets.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/images/**',
+      },
+    ],
+  },
+  experimental: {
+    outputFileTracing: true,
+    outputStandalone: true,
+    esmExternals: true,
+  },
+};
+
+const config = defineConfig(nextConfig);
+
+module.exports = config;
