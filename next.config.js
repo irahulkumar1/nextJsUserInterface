5a89d58c14c1f375de23dd96c1aa254e9c16327d
Rahul Kumar emp0123 <115157250+emp0123@users.noreply.github.com>
2024-03-22 22:37:59 +0530

    back to normal

--- next.config.js
+++ next.config.js
@@ -1,20 +1,12 @@
-// next.config.js
-import { defineConfig } from 'next/config';
-
-export default defineConfig({
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'sarkarifiling-assets.s3.ap-south-1.amazonaws.com',
-        port: '',
-        pathname: '/images/**',
-      },
-    ],
-  },
-  experimental: {
-    outputFileTracing: true,
-    outputStandalone: true,
-    esmExternals: true,
-  },
-});
+module.exports = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'sarkarifiling-assets.s3.ap-south-1.amazonaws.com',
+          port: '',
+          pathname: '/images/**',
+        },
+      ],
+    },
+  }
